e75823048e27ad333661470dbe5136611863cac4
Szwedzik <40271327+szwedzik@users.noreply.github.com>
2024-07-02 21:52:29 +0200

    added all files with correct line endings

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import Custom404 from "@/components/Errors/404/custom404";
+import Custom404 from "@/components/errors/404/custom404";
 
 export default function NotFoundPage() {
   return <Custom404 />;
