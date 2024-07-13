881d3326d98bf9633be5acf3652445ca0c378af4
Szwedzik <40271327+szwedzik@users.noreply.github.com>
2024-07-13 14:48:27 +0200

    updated server status component, updated homepage

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
     CFTOOL_URL: process.env.CFTOOL_URL,
     CFTOOL_APPID: process.env.CFTOOL_APPID,
     CFTOOL_SECRET: process.env.CFTOOL_SECRET,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 };
 
