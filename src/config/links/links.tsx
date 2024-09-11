45d2e7a4fee79d0be10c348e422debf2cff78b10
Paweł Łukasieiwcz <40271327+szwedzik@users.noreply.github.com>
2024-09-12 00:06:49 +0200

    updated lore url

--- src/config/links/links.tsx
+++ src/config/links/links.tsx
@@ -1,7 +1,8 @@
 const Links = {
   //rules: "/rules",
   rules: "https://docs.google.com/document/d/1k877UtXaE0tRQKLu0ZRSN5S6zJ2wQVi_dPaSiRWW2ZM",
-  lore: "/lore",
+  //lore: "/lore",
+  lore: "https://docs.google.com/document/d/134qcpYsMxOpf0fQruXTdWXgiD5yhPEV7Dei2Xr45FtI/edit",
   faq: "/faq",
   donate: "/donate",
   dashboard: "/dashboard",
