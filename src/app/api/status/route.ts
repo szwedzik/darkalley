f5e4f40ce8cd9d190b346924b8f1d26e011062db
Paweł Łukasieiwcz <40271327+szwedzik@users.noreply.github.com>
2024-11-04 21:55:27 +0100

    updated api/status route

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,31 +1,3 @@
-// export const runtime = 'edge';
-//
-// import { NextRequest, NextResponse } from "next/server";
-// import apiService from "@/lib/cfTools/apiServices";
-//
-// export const fetchCache = 'force-no-store';
-//
-// export async function GET(request: NextRequest) {
-//   const serverId = "9932ff9a812ed8d0faa5cf25a019ed264590bb33";
-//
-//   try {
-//     const serverInfo = await apiService.getServerInfo(serverId);
-//     console.log("fetching server info")
-//     if (!serverInfo) {
-//       return NextResponse.json(
-//         { error: "Failed to fetch server info 1" },
-//         { status: 500 },
-//       );
-//     }
-//     return NextResponse.json(serverInfo);
-//   } catch (error: any) {
-//     return NextResponse.json(
-//       { error: "Failed to fetch server info 2" },
-//       { status: 500 },
-//     );
-//   }
-// }
-
 export const runtime = 'edge';
 
 import { NextRequest, NextResponse } from "next/server";
@@ -37,7 +9,6 @@ export async function GET(request: NextRequest) {
   const serverId = "9932ff9a812ed8d0faa5cf25a019ed264590bb33";
 
   try {
-    // Fetch server information from the API
     const serverInfo = await apiService.getServerInfo(serverId);
     console.log("Fetching server info");
 
@@ -48,7 +19,6 @@ export async function GET(request: NextRequest) {
       );
     }
 
-    // Return the fetched server information as JSON
     return NextResponse.json(serverInfo);
   } catch (error) {
     console.error("Error fetching server info:", error);
